70f4493c7b74d7fcec220d6befcf289edb6035a7
ngosalwandoniza <ngosalwandoniza@gmail.com>
2026-02-03 08:48:51 +0200

    Fix CSRF admin error: allow HTTP cookies and add trusted origins for beta launch

--- latent_backend/settings.py
+++ latent_backend/settings.py
@@ -146,11 +146,14 @@ if not DEBUG:
     SECURE_HSTS_SECONDS = 31536000  # 1 year
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = False # Disabled for HTTP beta launch
+    CSRF_COOKIE_SECURE = False    # Disabled for HTTP beta launch
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
     X_FRAME_OPTIONS = 'DENY'
+
+    # Trusted origins for CSRF
+    CSRF_TRUSTED_ORIGINS = [f"http://{host}" for host in ALLOWED_HOSTS]
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
