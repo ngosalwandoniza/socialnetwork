5af407989b07585ffd9c817922d5b33dbcf58ba8
ngosalwandoniza <ngosalwandoniza@gmail.com>
2026-02-15 07:44:03 +0200

    Fix: Added missing dart:async import for Timer in ChatDetailScreen

--- latent_frontend/lib/screens/chat/chat_detail_screen.dart
+++ latent_frontend/lib/screens/chat/chat_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'dart:io';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
